c590c9dc5781eaa04948a4d7b4e0eefb3654647c
Rangel <icheckmyownmail@gmail.com>
2018-12-08 01:26:44 -0800

    A little bit of design Created two home pages (went with cards) Added a media folder to store the media. Added pictures to cards to create a better look created eventlistener to move to next page once card is clicked

--- clockSpecialtyClient/index.js
+++ clockSpecialtyClient/index.js
@@ -1,1 +1,19 @@
 console.log("index.js loaded");
+
+const cardSelection = document.querySelector('#cardContainer');
+
+
+cardSelection.addEventListener('click', function(event){
+	event.preventDefault();
+
+	let cardClicked = event.target.id;
+
+	if(cardClicked != undefined){
+
+		window.location.href = (cardClicked + ".html");
+	}
+
+	console.log("event Listener " + cardClicked);
+	
+
+})
