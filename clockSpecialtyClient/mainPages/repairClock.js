8d448307223881299512b3c2855a8652e78f64bb
Rangel <icheckmyownmail@gmail.com>
2018-12-17 13:22:57 -0800

    Turned repairClock.html into a FAQ page Added js code to link the href to the proper answer

--- clockSpecialtyClient/mainPages/repairClock.js
+++ clockSpecialtyClient/mainPages/repairClock.js
@@ -1,3 +1,8 @@
 console.log("in repair clock");
 
+//get the height of the main selector
+var mainContentHeight = document.querySelector("main").offsetHeight;
 
+var spacer = document.querySelector('#spacer');
+
+spacer.style.min-height = mainContentHeight;
